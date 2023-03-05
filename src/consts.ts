7efb556f0f29c0bac3e6de9a9b72c2f7b121143a
Joshua Levine <joshuaalevine3@gmail.com>
2023-03-04 21:57:29 -0500

    update project name where applicable

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'What I use - Joshua Levine';
+export const SITE_TITLE = 'Things I use - Joshua Levine';
 export const SITE_DESCRIPTION = 'A curated list of the tech I use';
 export const HOMEPAGE_URL = 'https://www.JoshuaLevine.me/';
 
